c51aa39f58dbd0f14f6d3f2214ffdfc39d7946a2
Johnny B <typngeek@aim.com>
2019-01-19 21:00:53 +0200

    day 1

---
+++ delay/delay.ino
@@ -0,0 +1,23 @@
+// assign pin connected to LED to a variable
+
+
+void setup() {
+  // set the pinMode of the LED pin as an output
+
+
+}
+
+void loop() {
+  // turn on the LED with digitalWrite
+  
+
+  // delay program for 500ms, LED stays on
+
+
+  // turn off the LED
+
+
+  // delay program for 500ms; LED stays off
+
+
+}
---
+++ digital_write/digital_write.ino
@@ -0,0 +1,11 @@
+// assign pin connected to LED to a variable
+
+
+void setup() {
+  // set the led_pin to be output
+}
+
+void loop() {
+  // set the pin high to turn on the LED forever
+
+}
