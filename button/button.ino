1e9fbe02aa40e171eb94ecd125af3d038622ceeb
Johnny B <typngeek@aim.com>
2019-01-21 10:03:08 +0200

    day2-fixed

--- button/button.ino
+++ button/button.ino
@@ -5,37 +5,35 @@ In this lab, you will be using DigitalWrite() in order to turn on and off your L
 
 */
 
-// define 'buttonPin' and 'ledPin' to be constant integers with a pin number
-const int buttonPin = 2;
-const int ledPin = 13;
+// define 'buttonPin' and 'ledPin' to be integers equal to 2 and 13, respectively
 
-// define an integer 'state' (which is NOT constant) that will hold the current state of the button
-int state = 0;
+
+
+// define an integer 'state' that will hold the current state of the button and set it equal to 0
+
 
 void setup() {
     // set the pin mode of 'buttonPin' as INPUT
-    pinMode(buttonPin, INPUT);
+
 
     // set the pin mode of 'ledPin' as OUTPUT
-    pinMode(ledPin, OUTPUT);
 
-    // begin serial communication
-    Serial.begin(9600);
+
+    // begin serial communication with baud rate 9600
+ 
 }
 
 void loop() {
     // update the 'state' variable with a digitalRead of the button
-    state = digitalRead(buttonPin);
+
     
-    Serial.println(state); //print out the state of the button
+    //print out the state of the button to the serial monitor
+    
     
     // use a conditional to check whether the state is HIGH, then digitalWrite HIGH to the ledPin
     // otherwise, set the digitalWrite to the ledPin to LOW
-    if (state == HIGH) {
-        digitalWrite(ledPin, HIGH);
-    } else {
-        digitalWrite(ledPin, LOW);
-    }
+
+
 
 }
 
